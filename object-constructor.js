13e15cb307083db93d0c140dca0b369818ec9b6c
majakwani <anasjakwani72@gmail.com>
2025-03-02 04:59:58 +0500

    Added Comments & explored prototype object

--- object-constructor.js
+++ object-constructor.js
@@ -1,13 +1,20 @@
+// All objects have a prototype
+
+// The objects we create inherit and can use stuff from prototype object.
+
 const Book = function(title, author, pages, bookRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.bookRead = bookRead === true ? "read" : "not read yet";
-    this.bookInfo = function(){
-        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.bookRead}.`;
-    }
+}
+
+Book.prototype.bookInfo = function(){
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.bookRead}.`;
 }
 
 let book1 = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
+let book2 = new Book("The Dwarf", "R.J.J. Tolkien", 592, true);
 
 console.log(book1.bookInfo());
+console.log(book2.bookInfo());
